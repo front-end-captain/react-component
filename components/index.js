d8a51621454d6009811f9f34e226c9ab273a8f60
yexianglei <18810940932@163.com>
2019-02-09 21:23:35 +0800

    :pencil:

--- components/index.js
+++ components/index.js
@@ -1,1 +1,6 @@
 export { default as Button } from "./Button/index.js";
+export { default as Message } from "./Message/index.js";
+export { default as Modal } from "./Modal/index.js";
+export { default as Progress } from "./Progress/index.js";
+export { default as Spin } from "./Spin/index.js";
+export { default as Tooltip } from "./Tooltip/index.js";
